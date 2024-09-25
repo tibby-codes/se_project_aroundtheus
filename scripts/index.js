d020081d2cc77077eb2053c40718ccc066c3ef36
Thomas Kastanowski <thomaskastanowski@thomass-air.lan>
2024-09-25 18:07:00 -0400

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -18,6 +18,7 @@ const initialCards = [
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
+
   {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
@@ -29,12 +30,48 @@ const initialCards = [
   },
 ];
 
-/* ------------------------- */
-
+/*-------------------------------------------------------------------------------*/
+/*                          Elements                                             */
+/*-------------------------------------------------------------------------------*/
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription =  document.querySelector('.profile__description');
+const profileTitleInput = document.querySelector('#profile-title-input');
+const profileDescriptionInput = document.querySelector('#profile-description-input');
 
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+
+/*--------------------------------------------------------------------------------*/
+/*                            Functions                                           */
+/*--------------------------------------------------------------------------------*/
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+/*---------------------------------------------------------------------------------*/
+/*                             Event Handlers                                      */
+/*----------------------------------------------------------------------------------*/
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value
+  profileDescription.textContent = profileDescriptionInput.value; 
+  closePopup();
+}
+
+/*---------------------------------------------------------------------------------*/
+/*                             Event Listeners                                     */
+/*----------------------------------------------------------------------------------*/
 
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent; 
+  profileEditModal.classList.add("modal__opened");
 });
+
+profileEditCloseButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
