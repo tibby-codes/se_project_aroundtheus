eb7402a042c4ad2951abd871b068300ae1cab974
Thomas Kastanowski <thomaskastanowski@thomass-air.lan>
2024-09-30 19:36:33 -0400

    First submission

--- scripts/index.js
+++ scripts/index.js
@@ -40,17 +40,31 @@ const profileTitle = document.querySelector(".profile__title");
 const profileDescription =  document.querySelector('.profile__description');
 const profileTitleInput = document.querySelector('#profile-title-input');
 const profileDescriptionInput = document.querySelector('#profile-description-input');
-
 const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+
 
 /*--------------------------------------------------------------------------------*/
 /*                            Functions                                           */
 /*--------------------------------------------------------------------------------*/
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove(".modal__opened");
 }
 
+function getCardElement(cardData) {
+const cardElement = cardTemplate.cloneNode(true);
+
+const cardImageEl = cardElement.querySelector(".card__image");
+const cardTitleEl = cardElement.querySelector(".card__title");
+
+cardImageEl.src = cardData.link;
+cardImageEl.alt = cardData.name;
+cardTitleEl.textContent = cardData.name; 
+
+return cardElement;
+}
 /*---------------------------------------------------------------------------------*/
 /*                             Event Handlers                                      */
 /*----------------------------------------------------------------------------------*/
@@ -75,3 +89,7 @@ profileEditCloseButton.addEventListener("click", closePopup);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
 
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
+});
